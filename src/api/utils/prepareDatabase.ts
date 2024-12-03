be694c7e15084ced74b918acda3da19e23292be8
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-12-03 16:24:40 +1000

    add sqlite database

--- src/api/utils/prepareDatabase.ts
+++ src/api/utils/prepareDatabase.ts
@@ -6,5 +6,34 @@ export function prepareDatabase(): Database {
         create: true // create if does not exist 
     });
 
+    // create chores table
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS chores (
+            id INTEGER NOT NULL PRIMARY KEY, 
+            label VARCHAR(60) NOT NULL UNIQUE, 
+            dayToReset DATETIME, 
+            daysToKeep INTEGER, 
+            done BOOLEAN NOT NULL
+        );  
+    `);
+
+    // create default chores                
+    db.exec(`
+        REPLACE INTO chores (
+            label, dayToReset, daysToKeep, done
+        ) VALUES (
+            'Water the house plants',
+            datetime('now', '+1 day'),
+            1,
+            false       
+        ),
+        (
+            'Feed the cat',
+            datetime('now', '+1 day'),
+            1,
+            false
+        );
+    `)
+
     return db;
-}
+}   
