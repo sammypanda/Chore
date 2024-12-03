a1d488bf07509d01553e329f6f0ce07ff7989e81
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-12-03 16:32:00 +1000

    integrate getChoreList with database

--- src/api/endpoints/getChoreList.ts
+++ src/api/endpoints/getChoreList.ts
@@ -1,24 +1,15 @@
 import type { Context } from "@oak/oak";
+import { getDatabase } from "../main.ts";
 
-export function getChoreList(context: Context) {
-    // TODO: grab this data from persistent storage, like a database
-    const chores = [{
-        "id": 0,
-        "label": "Water the plants",
-        "dayToReset": 1733407200,
-        "daysToKeep": 3,
-        "done": true
-    },
-    {
-        "id": 1,
-        "label": "Feed the cat",
-        "dayToReset": 1733234400,
-        "daysToKeep": 1,
-        "done": false
-    }];
+export async function getChoreList(context: Context) {
+    const db = getDatabase();
 
-    context.response.body = { 
+    // get all the chores
+    const chores = await db.prepare("SELECT * FROM chores").all();
+
+    // send response
+    context.response.body = {   
         message: "The following are the chores, their id (integer), their label (string), the day to reset the state (unix timestamp), the amount of days to keep (integer) and their current state (boolean)", 
-        chores 
+        chores
     };
 };
